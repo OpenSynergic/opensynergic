bc934eb4f9a6810fa489f02e6faefe4f821e748f
Rahman Ramsi <rahmanramsi19@gmail.com>
2022-06-24 15:04:47 +0800

    update monorepo-builder

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -24,10 +24,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     # release workers - in order to execute
     $services->set(UpdateReplaceReleaseWorker::class);
-    // $services->set(SetCurrentMutualDependenciesReleaseWorker::class);
     $services->set(TagVersionReleaseWorker::class);
     $services->set(PushTagReleaseWorker::class);
-    // $services->set(SetNextMutualDependenciesReleaseWorker::class);
-    $services->set(UpdateBranchAliasReleaseWorker::class);
-    $services->set(PushNextDevReleaseWorker::class);
 };
