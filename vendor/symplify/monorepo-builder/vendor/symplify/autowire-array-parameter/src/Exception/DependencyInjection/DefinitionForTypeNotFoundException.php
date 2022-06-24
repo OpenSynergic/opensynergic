1ba02ca2b980a17a15163172d1deb49b17f6f216
Rahman Ramsi <rahmanramsi19@gmail.com>
2022-06-24 11:10:04 +0800

    remove vendor

--- vendor/symplify/monorepo-builder/vendor/symplify/autowire-array-parameter/src/Exception/DependencyInjection/DefinitionForTypeNotFoundException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare (strict_types=1);
-namespace MonorepoBuilder202206\Symplify\AutowireArrayParameter\Exception\DependencyInjection;
-
-use Exception;
-final class DefinitionForTypeNotFoundException extends Exception
-{
-}
--- vendor/symplify/monorepo-builder/vendor/symplify/package-builder/src/Exception/MissingPrivatePropertyException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare (strict_types=1);
-namespace MonorepoBuilder202206\Symplify\PackageBuilder\Exception;
-
-use Exception;
-final class MissingPrivatePropertyException extends Exception
-{
-}
